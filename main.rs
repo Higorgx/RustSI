e4c1bf5e99c05e560a2f9d6ed3056a367643b0c7
Higorgx <36903838+Higorgx@users.noreply.github.com>
2019-05-14 15:29:03 -0300

    Update main.rs

--- main.rs
+++ main.rs
@@ -1,35 +1,50 @@
-
-fn main(){
-	let  p1 = 1.0;
-	let  ma1 = 1.0;
-	let  mb1 = 1.0;
-	let  p2 = 0.0;
-	let  ma2 = 0.0;
-	let  mb2 = 0.0;
-	let  qda = 0.0; 
-	let  qdf = 0.0;
-
-	let  a1 = (p1 * 0.7) + (ma1 * 0.2) + (mb1 * 0.1);
-	let  a2 = (p2 * 0.7) + (ma2 * 0.2) + (mb2 * 0.1);
-	let mf = (a1 + 2.00 * a2) / 3.00;
-	
-	let presenca = qda / 100.00 * 75.00;
-	if presenca >= qdf {
-		if mf < 5.00 && mf >= 3.00{
-		println!("Recuperacao");
-		}
-		if mf <3.00 {
-		println!("Reprovado por nota");
-		}
-		if mf >= 5.00 {
-		println!("Aprovado");
-		}
-	}
-	else{
-	println!("Reprovado por falta");
-	}
-	
-	println!("The value of A1 is: {}", mf);
-	
-	
+fn main() {
+    let  p1 = 1.0;
+    let  ma1 = 5.0;
+    let  ma2 = 5.0;
+    let  p2 = 1.0;
+    let  mb1 = 5.0;
+    let  mb2 = 5.0;
+    let  aulas = 30.0;
+    let  faltas = 25.0;
+    
+    
+    let media1 = p1*0.7+ma1*0.20+ma2*0.10;
+    let media2 = p2*0.7+mb1*0.20+mb2*0.10;
+    
+    let mediafinal =(media1+(2.0*media2))/3.0;
+    
+    println!("Nota da P1: {}", p1);
+    println!("Nota do Trabalho: {}", ma1);
+    println!("Nota do Trabalho: {}", ma2);
+    println!("Media: {}", media1);
+    println!("");
+    
+    println!("Nota da P2: {}", p2);
+    println!("Nota do Trabalho: {}", mb1);
+    println!("Nota do Trabalho: {}", mb2);
+    println!("Media: {}", media2);
+    println!("");
+    println!("Media Final: {}", mediafinal);
+    println!("");
+    
+    println!("Quantidade de aulas: {}", aulas);
+    println!("Quantidade de faltas: {}", faltas);
+    let frequencia = (faltas/aulas)*100.0;
+    println!("Frequencia: {}%", frequencia);
+    println!("");
+    
+    
+    if frequencia>=75.0 {
+        if mediafinal >= 5.0 {
+            print!("Aprovado");
+        }else if mediafinal < 5.0 && mediafinal >= 3.0{
+                print!("Recuperação");
+        } else { 
+            print!("Reprovado por nota");
+        }
+    
+    } else {
+        print!("Reprovado por frequência");
+    }
 }
